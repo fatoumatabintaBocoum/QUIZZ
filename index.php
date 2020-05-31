8042be609ae8ae430dd18618fe473f42df2dd6f7
fatoumatabintaBocoum <fatoumata-binta.bocoum@ism.edu.sn>
2020-05-31 20:41:05 +0200

    quizz

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+   define("URL_ASSETS","http://localhost/bases/quizzs/assets");
+
+  require_once('./libs/Router.php');
+   $router=new Router();
+   //controller/methode=>UC
+   $router->getRoute();
+   /*
+   $sec=new Security();
+   $sec->showPage();
+   $obj->{$method}()
+   */
+?>
